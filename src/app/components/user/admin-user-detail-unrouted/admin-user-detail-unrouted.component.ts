8bb21cf54f94af35ba1233bb7a2f9e922e40dea2
AlanMcLure <alanmclurealarcon02@gmail.com>
2023-10-23 22:06:51 +0200

    arreglo dialog

--- src/app/components/user/admin-user-detail-unrouted/admin-user-detail-unrouted.component.ts
+++ src/app/components/user/admin-user-detail-unrouted/admin-user-detail-unrouted.component.ts
@@ -1,5 +1,6 @@
 import { HttpClient, HttpErrorResponse } from '@angular/common/http';
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, Input, OnInit, Optional } from '@angular/core';
+import { DynamicDialogConfig, DynamicDialogRef } from 'primeng/dynamicdialog';
 import { IUser } from 'src/app/model/model.interfaces';
 
 @Component({
@@ -12,14 +13,22 @@ export class AdminUserDetailUnroutedComponent implements OnInit {
  
   @Input() id: number = 1;
 
-  textoDeEntrada: string = "";
+  // textoDeEntrada: string = "";
   oUser: IUser | null = null;
   status: HttpErrorResponse | null = null;
   
 
   constructor(
-    private oHttpClient: HttpClient
-  ) { }
+    private oHttpClient: HttpClient,
+    @Optional() public ref:DynamicDialogRef,
+    @Optional() public config:DynamicDialogConfig
+  ) {     
+    if (config){
+      if (config.data){
+        this.id = config.data.id;
+      }
+    }    
+  }
 
   ngOnInit() {
     this.getOne();
@@ -38,24 +47,24 @@ export class AdminUserDetailUnroutedComponent implements OnInit {
 
   }
 
-  onTextoDeEntradaChange(): void {
-    const userId = parseInt(this.textoDeEntrada, 10); // Convierte el texto a un número entero
+  // onTextoDeEntradaChange(): void {
+  //   const userId = parseInt(this.textoDeEntrada, 10); // Convierte el texto a un número entero
 
-    if (!isNaN(userId)) {
-      // Realiza una solicitud a la API con el ID ingresado
-      this.oHttpClient.get<IUser>("http://localhost:8083/user/" + userId).subscribe({
-      next: (data: IUser) => {
-        this.oUser = data;
-      },
-      error: (error: any) => {
-        this.oUser = null;
-      }
+  //   if (!isNaN(userId)) {
+  //     // Realiza una solicitud a la API con el ID ingresado
+  //     this.oHttpClient.get<IUser>("http://localhost:8083/user/" + userId).subscribe({
+  //     next: (data: IUser) => {
+  //       this.oUser = data;
+  //     },
+  //     error: (error: any) => {
+  //       this.oUser = null;
+  //     }
 
-    })
-    } else {
-      // Si el valor no es un número válido, borra los datos
-      this.oUser = null;
-    }
-  }
+  //   })
+  //   } else {
+  //     // Si el valor no es un número válido, borra los datos
+  //     this.oUser = null;
+  //   }
+  // }
 
 }
